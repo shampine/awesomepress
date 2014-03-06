a6bb02ffd63f1bd6a7ce4e14c3b86a55481b9b72
Patrick Shampine <pshampine@gunnjerkens.com>
2014-03-06 15:03:05 -0800

    Wrapped up in superAwesome

--- awesomepress.php
+++ awesomepress.php
@@ -16,18 +16,29 @@ See Font Awesome for more: http://fortawesome.github.io/Font-Awesome/get-started
 */
 
 
-function register_font_awesome() {
-  wp_enqueue_style('font-awesome', 'http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css', false, null, false);
-}
-add_action('get_header','register_font_awesome');
+class superAwesome {
 
-function awesome_codes($atts) {
-  extract(shortcode_atts(array(
-    'font'  => '',
-  ), $atts));
-  $awesome = '<i class="fa '.$font.'"></i>';
-  return $awesome;
-}
-add_shortcode( 'fa', 'awesome_codes' );
+  public function __construct() {
+      add_action( 'init', array( &$this, 'init' ) );
+  }
 
-?>
+  public function init() {
+    add_action( 'wp_enqueue_scripts', array( $this, 'register_font_awesome' ) );
+    add_shortcode( 'fa', array( $this, 'awesome_codes' ) );
+  }
+
+  public function register_font_awesome() {
+    wp_enqueue_style('font-awesome', 'http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css', false, null, false);
+  }
+
+  public function awesome_codes($atts) {
+    extract(shortcode_atts(array(
+      'font'  => '',
+    ), $atts));
+    $awesome = '<i class="fa '.$font.'"></i>';
+    return $awesome;
+  }
+
+}
+
+new superAwesome();
