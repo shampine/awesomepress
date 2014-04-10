99ea4ffd0eb9c21053aa58741744d038c66788b2
Patrick Shampine <pshampine@gunnjerkens.com>
2014-04-10 12:52:35 -0700

    Name did not work

--- awesomepress.php
+++ awesomepress.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: AwesomePress
+Plugin Name: awesomepress-gh
 Plugin URI: https://github.com/patrickshampine/awesomepress
 Description: Embed Font Awesome (http://fontawesome.io) from the Bootstrap CDN, maybe allow shortcode usage in WSYIAWESOME?
 Version: 0.1
